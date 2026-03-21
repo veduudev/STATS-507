e2eae64069cb11f2af7fc24878e8df2ab96c328f
sili-gato <vedaansh.vijaywargiya@gmail.com>
2026-03-21 00:18:32 -0400

    moved project to new directory

--- project/src/models/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Model modules for heterogeneous GNN gene-disease prediction."""
-
-from src.models.rgcn_model import RGCNEncoder
-from src.models.hgt_model import HGTEncoder
-from src.models.decoder import DistMultDecoder
-
-__all__ = ["RGCNEncoder", "HGTEncoder", "DistMultDecoder"]
